c663ff06bbd7f769ce6a02c092fb138fa4c42c7e
laurent <laurent.mazare@gmail.com>
2024-08-31 11:23:13 +0200

    MLX weights import.

---
+++ scripts/import_mlx.py
@@ -0,0 +1,97 @@
+# Copyright (c) Kyutai, all rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import torch
+from pathlib import Path
+from safetensors.torch import save_file
+
+
+def import_model(in_path: Path, out_path: Path, silent: bool = False) -> None:
+    pkg = torch.load(in_path, map_location=torch.device("cpu"))
+    tch_model = pkg["fsdp_best_state"]["model"]
+
+    n_q: int | None = None
+    for idx in range(999):
+        name = f"emb.{idx}.weight"
+        if name not in tch_model:
+            n_q = idx
+            break
+    assert n_q is not None
+    if not silent:
+        print("Found n_q:", n_q)
+
+    model = {}
+    for name in ["text_emb.weight", "text_linear.weight"]:
+        model[name] = tch_model[name]
+    model["out_norm.weight"] = tch_model["out_norm.alpha"][0, 0]
+    for idx in range(n_q):
+        src_name = f"emb.{idx}.weight"
+        dst_name = f"audio_embs.{idx}.weight"
+        model[dst_name] = tch_model[src_name]
+
+    for k, v in sorted(tch_model.items()):
+        if not silent:
+            print(k, v.shape, v.dtype)
+        if k.startswith("transformer"):
+            if k.endswith(".alpha"):
+                v = v[0, 0]
+            k = k.replace(".alpha", ".weight")
+            k = k.replace(".in_proj_weight", ".in_proj.weight")
+            model[k] = v
+
+    # Only export the first 8 slices of the depformer (main).
+    for idx in range(min(8, n_q)):
+        base = f"depformer.slices.{idx}."
+        model[base + "linear_in.weight"] = tch_model[f"depformer_in.{idx}.weight"]
+        model[base + "linear_out.weight"] = tch_model[f"linears.{idx}.weight"]
+        if idx == 0:
+            model[base + "emb.weight"] = tch_model["depformer_text_emb.weight"]
+        else:
+            model[base + "emb.weight"] = tch_model[f"depformer_emb.{idx-1}.weight"]
+
+        for layer_idx in range(6):
+            layer = base + f"transformer.layers.{layer_idx}."
+            # WARNING: note that this uses in_proj_weight vs out_proj.weight
+            model[layer + "self_attn.in_proj.weight"] = (
+                tch_model[f"depformer.layers.{layer_idx}.self_attn.in_proj_weight"]
+                .chunk(n_q)[idx]
+                .clone()
+            )
+            model[layer + "self_attn.out_proj.weight"] = (
+                tch_model[f"depformer.layers.{layer_idx}.self_attn.out_proj.weight"]
+                .chunk(n_q)[idx]
+                .clone()
+            )
+            model[layer + "norm1.weight"] = tch_model[
+                f"depformer.layers.{layer_idx}.norm1.alpha"
+            ][0, 0].clone()
+            model[layer + "norm2.weight"] = tch_model[
+                f"depformer.layers.{layer_idx}.norm2.alpha"
+            ][0, 0].clone()
+            model[layer + "gating.linear_in.weight"] = tch_model[
+                f"depformer.layers.{layer_idx}.gating.{idx}.linear_in.weight"
+            ]
+            model[layer + "gating.linear_out.weight"] = tch_model[
+                f"depformer.layers.{layer_idx}.gating.{idx}.linear_out.weight"
+            ]
+
+    save_file(model, out_path)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('checkpoint', type=str, help="the pytorch checkpoint to import")
+    parser.add_argument('out', type=str, help="the mlx safetensors file to generate")
+    parser.add_argument('-s', '--silent', action='store_true', help="Only prints the checkpoint name")
+    args = parser.parse_args()
+
+    ckpt_path = Path(args.checkpoint)
+    out_path = Path(args.out)
+    if not out_path.exists():
+        import_model(ckpt_path, out_path, silent=args.silent)
+    print(out_path)
+
+if __name__ == "__main__":
+    main()
--- scripts/test_mlx.py
+++ scripts/test_mlx.py
@@ -2,50 +2,70 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from dataclasses import dataclass
+import argparse
 import time
-from typing import List, Optional, Tuple
+from pathlib import Path
 import sentencepiece
 
 import mlx.core as mx
-import mlx.nn as nn
+from mlx.utils import tree_map_with_path
 
 import msh_mlx
 
-STEPS = 1000
-MODEL_FILE = "/Users/kyutai/tmp/mimi_0abbed5f@100.safetensors"
-TOKENIZER_FILE = "/Users/kyutai/tmp/tokenizer_spm_32k_3.model"
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tokenizer", type=str)
+    parser.add_argument("--model", type=str)
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--steps", default=100, type=int)
+    args = parser.parse_args()
+    
+    model_file = args.model
+    tokenizer_file = args.tokenizer
+    if model_file is None:
+        model_file = str(Path.home() / "tmp/" / "mimi_0abbed5f@100.safetensors")
+    if tokenizer_file is None:
+        tokenizer_file = str(Path.home() / "tmp" / "tokenizer_spm_32k_3.model")
 
-text_tokenizer = sentencepiece.SentencePieceProcessor(TOKENIZER_FILE)
-mx.random.seed(299792458)
+    
+    print(f"loading text tokenizer {tokenizer_file}")
+    text_tokenizer = sentencepiece.SentencePieceProcessor(tokenizer_file)
+    mx.random.seed(299792458)
+    
+    lm_config = msh_mlx.models.config_v0_1()
+    if args.verbose:
+        print(f"model config:\n{lm_config}")
+    
+    model = msh_mlx.models.Lm(lm_config)
+    model.set_dtype(mx.bfloat16)
+    # nn.quantize(model, bits=8)
 
-lm_config = msh_mlx.models.config_v0_1()
+    if args.verbose:
+        tree_map_with_path(lambda p, t: print(p, t.shape), model.parameters())
 
-model = msh_mlx.models.Lm(lm_config)
-print(model)
-print(lm_config)
-model.set_dtype(mx.bfloat16)
-print("model created")
-# nn.quantize(model, bits=8)
-print("model quantized, loading weights...")
-model.load_weights(MODEL_FILE, strict=False)
-print("weights loaded")
+    print(f"loading weights {model_file}")
+    model.load_weights(model_file, strict=True)
+    print("weights loaded")
+    
+    cache = None
+    start_time = 0
+    last_token = mx.array([[32000]])
+    sampler = msh_mlx.utils.Sampler()
+    for i in range(args.steps + 1):
+        if i == 1:
+            start_time = time.time()
+        logits, cache = model(last_token, cache)
+        last_token, _ = sampler(logits[:, 0])
+        text_token = last_token[0].item()
+        if text_token not in (0, 3):
+            _text = text_tokenizer.id_to_piece(text_token)
+            _text = _text.replace("▁", " ")
+            print(_text, end='', flush=True)
+    
+        last_token = last_token[None]
+    print()
+    token_per_second = args.steps / (time.time() - start_time)
+    print(f"steps: {args.steps}, token per sec: {token_per_second}")
 
-cache = None
-start_time = 0
-last_token = mx.array([[32000]])
-sampler = msh_mlx.utils.Sampler()
-for i in range(STEPS + 1):
-    if i == 1:
-        start_time = time.time()
-    logits, cache = model(last_token, cache)
-    last_token, _ = sampler(logits[:, 0])
-    text_token = last_token[0].item()
-    if text_token not in (0, 3):
-        _text = text_tokenizer.id_to_piece(text_token)
-        _text = _text.replace("▁", " ")
-        print(_text, end='', flush=True)
-
-    last_token = last_token[None]
-token_per_second = STEPS / (time.time() - start_time)
-print(f"steps: {STEPS}, token per sec: {token_per_second}")
+if __name__ == "__main__":
+    main()
