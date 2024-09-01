842a70f9f16ed60637bb6c2d965b8d1066309938
Laurent <laurent.mazare@gmail.com>
2024-09-01 20:05:59 +0200

    Remove the encodec bits for now.

--- msh_mlx/models/__init__.py
+++ msh_mlx/models/__init__.py
@@ -5,9 +5,5 @@
 Models for EnCodec, AudioGen, MusicGen, as well as the generic LMModel.
 """
 
-from .encodec import (
-    CompressionModel,
-    EncodecModel,
-)
 from .lm import Lm, LmConfig, config_v0_1
 from .generate import LmGen
--- msh_mlx/modules/__init__.py
+++ msh_mlx/modules/__init__.py
@@ -3,14 +3,4 @@
 # LICENSE file in the root directory of this source tree.
 """Modules used for building the models."""
 
-from .conv import (
-    NormConv1d,
-    NormConvTranspose1d,
-    StreamableConv1d,
-    StreamableConvTranspose1d,
-    pad_for_conv1d,
-    pad1d,
-    unpad1d,
-)
-from .seanet import SeaNetEncoder, SeaNetDecoder
 from .transformer import Transformer, TransformerConfig
