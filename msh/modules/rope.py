b7364d1f2b4f750f265d37fedbf62916f3b913b3
laurent <laurent.mazare@gmail.com>
2024-09-03 10:27:04 +0200

    cuda-graph the rope computation.

--- msh/modules/rope.py
+++ msh/modules/rope.py
@@ -64,10 +64,68 @@ class RotaryEmbedding(nn.Module):
         max_period (float): Maximum period of the rotation frequencies.
     """
 
-    def __init__(self, max_period: float = 10000.0):
+    def __init__(
+        self, dim: int, max_len: int, device=None, max_period: float = 10000.0
+    ):
         super().__init__()
+
+        assert dim > 0
+        assert dim % 2 == 0
+        assert max_period > 0
+
         self.max_period = max_period
+        self.max_len = max_len
+        self.dim = dim
+
+        ds = torch.arange(dim // 2, device=device, dtype=torch.float32)
+        freqs = torch.exp(ds * (-math.log(max_period) * 2 / dim))
+        ts = torch.arange(max_len, device=device, dtype=torch.float32).view(-1, 1, 1)
+        self.rotr = torch.cos(freqs * ts)
+        self.roti = torch.sin(freqs * ts)
+        self.cuda_graph = None
 
     def forward(self, q: torch.Tensor, k: torch.Tensor, offset: int = 0):
         """Apply rope rotation to query or key tensor."""
+        B, T, H, D = q.shape
+        if T == 1:
+            if self.cuda_graph is None:
+                self.cuda_graph = torch.cuda.CUDAGraph()
+                self.in_q = q.clone()
+                self.in_k = k.clone()
+                self.in_rotr = self.rotr[offset : offset + T].clone()
+                self.in_roti = self.roti[offset : offset + T].clone()
+                self.current_offset = offset
+                with torch.cuda.graph(self.cuda_graph):
+                    q = self.in_q.view(B, T, H, D // 2, 2)
+                    k = self.in_k.view(B, T, H, D // 2, 2)
+
+                    # convention is `r` suffix is real part, `i` is imaginary.
+                    qr = q[..., 0].float()
+                    qi = q[..., 1].float()
+
+                    kr = k[..., 0].float()
+                    ki = k[..., 1].float()
+
+                    qor = qr * self.in_rotr - qi * self.in_roti
+                    qoi = qr * self.in_roti + qi * self.in_rotr
+
+                    kor = kr * self.in_rotr - ki * self.in_roti
+                    koi = kr * self.in_roti + ki * self.in_rotr
+
+                    dtype = q.dtype
+                    qo = torch.stack([qor.to(dtype), qoi.to(dtype)], dim=-1)
+                    ko = torch.stack([kor.to(dtype), koi.to(dtype)], dim=-1)
+
+                    self.out_q = qo.view(B, T, H, D)
+                    self.out_k = ko.view(B, T, H, D)
+            else:
+                self.in_q.copy_(q)
+                self.in_k.copy_(k)
+                if self.current_offset != offset:
+                    self.in_rotr.copy_(self.rotr[offset : offset + T])
+                    self.in_roti.copy_(self.roti[offset : offset + T])
+                    self.current_offset = offset
+            self.cuda_graph.replay()
+            return (self.out_q, self.out_k)
+
         return apply_rope(q, k, offset, self.max_period)
