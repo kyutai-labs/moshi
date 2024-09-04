cd036e3973cd7132ed67abb3ce519e0fa48b6940
Alexandre Défossez <alexandre.defossez@gmail.com>
2024-09-04 20:38:35 +0200

    initial

--- client_opus.py
+++ client_opus.py
@@ -28,42 +28,95 @@ def colorize(text, color):
     return "".join([code, text, restore])
 
 
-def log(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+def make_log(level: str, msg: str) -> str:
+    if level == "warning":
+        prefix = colorize("Warning:", "1;31")
+    elif level == "info":
+        prefix = colorize("Info:", "1;34")
+    elif level == "error":
+        prefix = colorize("Error:", "1;31")
+    else:
+        raise ValueError(f"Unknown level {level}")
+    return prefix + ' ' + msg
 
 
-class TokenPrinter:
-    def __init__(self, max_cols: int = 80, stream=sys.stdout):
+class RawPrinter:
+    def __init__(self, stream=sys.stdout, err_stream=sys.stderr):
+        self.stream = stream
+        self.err_stream = err_stream
+
+    def print_header(self):
+        pass
+
+    def print_token(self, token: str):
+        self.stream.write(token)
+        self.stream.flush()
+
+    def log(self, level: str, msg: str):
+        print(f"{level.capitalize()}: {msg}", file=self.err_stream)
+
+    def print_lag(self):
+        self.err_stream.write(colorize(' [LAG]', '31'))
+        self.err_stream.flush()
+
+    def print_pending(self):
+        pass
+
+
+class Printer:
+    def __init__(self, max_cols: int = 80, stream=sys.stdout, err_stream=sys.stderr):
         self.max_cols = max_cols
         self.current_line: str = ''
         self.stream = stream
+        self.err_stream = err_stream
         self._pending_count = 0
         self._pending_printed = False
 
     def print_header(self):
         print('-' * self.max_cols)
 
-    def print_token(self, token: str):
+    def _remove_pending(self) -> bool:
         if self._pending_printed:
             self._pending_printed = False
             self.stream.write('\r' + self.current_line + ' ' * 2)
+            return True
+        return False
+
+    def print_token(self, token: str):
+        self._remove_pending()
         if len(self.current_line) + len(token) <= self.max_cols:
             self.stream.write(token)
             self.current_line += token
         else:
             if token.startswith(' '):
+                token = token.lstrip()
                 self.stream.write('\n' + token)
                 self.current_line = token
             else:
                 if ' ' in self.current_line:
                     line, word = self.current_line.rsplit(' ', 1)
-                    sys.stdout.write('\r' + line + ' ' * (len(word) + 1) + '\n' + word + token)
-                    self.current_line = word + token
+                    token = word.lstrip() + token
+                    sys.stdout.write('\r' + line + ' ' * (len(word) + 1) + '\n' + token)
+                    self.current_line = token
                 else:
                     remaining = self.max_cols - len(self.current_line)
                     sys.stdout.write(token[:remaining] + '\n' + token[remaining:])
                     self.current_line = token[remaining:]
         self.stream.flush()
+
+    def log(self, level: str, msg: str):
+        msg = make_log(level, msg)
+        removed = self._remove_pending()
+        if self.current_line:
+            self.stream.write('\n')
+        elif removed:
+            self.stream.write('\t')
+        self.stream.flush()
+        print(msg, file=self.err_stream)
+        self.err_stream.flush()
+
+    def print_lag(self):
+        self.print_token(colorize(' [LAG]', '31'))
 
     def print_pending(self):
         chars = ['|', '/', '-', '\\']
@@ -78,87 +131,110 @@ class TokenPrinter:
 
 
 async def main():
-    uri = f"ws://{args.host}:{args.port}"
-    log(f"connecting to {uri}")
+    async def queue_loop():
+        while True:
+            if done:
+                return
+            await asyncio.sleep(0.001)
+            msg = opus_writer.read_bytes()
+            if len(msg) > 0:
+                try:
+                    await websocket.send(b"\x01" + msg)
+                except websockets.exceptions.WebSocketException:
+                    return
 
+    async def decoder_loop():
+        all_pcm_data = None
+        while True:
+            if done:
+                return
+            await asyncio.sleep(0.001)
+            pcm = opus_reader.read_pcm()
+            if all_pcm_data is None:
+                all_pcm_data = pcm
+            else:
+                all_pcm_data = np.concatenate((all_pcm_data, pcm))
+            while all_pcm_data.shape[-1] >= FRAME_SIZE:
+                output_queue.put(all_pcm_data[:FRAME_SIZE])
+                all_pcm_data = np.array(all_pcm_data[FRAME_SIZE:])
+
+    async def recv_loop():
+        nonlocal done
+        cnt = 0
+        while True:
+            try:
+                message = await websocket.recv()
+            except websockets.exceptions.WebSocketException:
+                printer.log("error", "Lost connection with the server!")
+                done = True
+                return
+            if not isinstance(message, bytes):
+                printer.log("warning", f"unsupported message type {type(message)}")
+                continue
+            if len(message) == 0:
+                printer.log("warning", "empty message")
+                continue
+            kind = message[0]
+            if kind == 1:  # audio
+                payload = message[1:]
+                opus_reader.append_bytes(payload)
+                printer.print_pending()
+                cnt += 1
+            elif kind == 2:  # text
+                payload = message[1:]
+                printer.print_token(payload.decode())
+            else:
+                printer.log("warning", f"unknown message kind {kind}")
+
+    def on_input(in_data, frames, time_, status):
+        assert in_data.shape[0] == FRAME_SIZE
+        opus_writer.append_pcm(in_data[:, 0])
+
+    def on_output(out_data, frames, time_, status):
+        assert out_data.shape == (FRAME_SIZE, 1), out_data.shape
+        try:
+            pcm_data = output_queue.get(block=False)
+            # TODO: handle other shapes by using some form of fifo/ring buffer.
+            assert pcm_data.shape == (FRAME_SIZE,), pcm_data.shape
+            out_data[:, 0] = pcm_data
+        except queue.Empty:
+            out_data.fill(0)
+            printer.print_lag()
+
+    uri = f"ws://{args.host}:{args.port}"
+    printer: Printer | RawPrinter
+    if sys.stdout.isatty():
+        printer = Printer()
+    else:
+        printer = RawPrinter()
+    printer.log("info", f"connecting to {uri}")
+
+    try:
+        connect = websockets.connect(uri)
+        websocket = await connect.__aenter__()
+    except websockets.exceptions.WebSocketException:
+        printer.log("error", "Failed to connect!")
+        sys.exit(1)
+
+    printer.log("info", "connected!")
+    in_stream = sd.InputStream(
+        samplerate=SAMPLE_RATE, channels=CHANNELS, blocksize=FRAME_SIZE, callback=on_input
+    )
+
+    out_stream = sd.OutputStream(
+        samplerate=SAMPLE_RATE,
+        channels=CHANNELS,
+        blocksize=FRAME_SIZE,
+        callback=on_output,
+    )
     opus_writer = sphn.OpusStreamWriter(SAMPLE_RATE)
     opus_reader = sphn.OpusStreamReader(SAMPLE_RATE)
     output_queue = queue.Queue()
-    printer = TokenPrinter()
+    done = False
+    printer.print_header()
 
-    async with websockets.connect(uri) as websocket:
-
-        async def queue_loop():
-            while True:
-                await asyncio.sleep(0.001)
-                msg = opus_writer.read_bytes()
-                if len(msg) > 0:
-                    await websocket.send(b"\x01" + msg)
-
-        async def decoder_loop():
-            all_pcm_data = None
-            while True:
-                await asyncio.sleep(0.001)
-                pcm = opus_reader.read_pcm()
-                if all_pcm_data is None:
-                    all_pcm_data = pcm
-                else:
-                    all_pcm_data = np.concatenate((all_pcm_data, pcm))
-                while all_pcm_data.shape[-1] >= FRAME_SIZE:
-                    output_queue.put(all_pcm_data[:FRAME_SIZE])
-                    all_pcm_data = np.array(all_pcm_data[FRAME_SIZE:])
-
-        async def recv_loop():
-            log("start recv loop")
-            printer.print_header()
-            cnt = 0
-            while True:
-                message = await websocket.recv()
-                if not isinstance(message, bytes):
-                    log(f"unsupported message type {type(message)}")
-                    continue
-                if len(message) == 0:
-                    log("empty message")
-                    continue
-                kind = message[0]
-                if kind == 1:  # audio
-                    payload = message[1:]
-                    opus_reader.append_bytes(payload)
-                    printer.print_pending()
-                    cnt += 1
-                elif kind == 2:  # text
-                    payload = message[1:]
-                    printer.print_token(payload.decode())
-                else:
-                    log(f"unknown message kind {kind}")
-
-        def on_input(in_data, frames, time_, status):
-            assert in_data.shape[0] == FRAME_SIZE
-            opus_writer.append_pcm(in_data[:, 0])
-
-        in_stream = sd.InputStream(
-            samplerate=SAMPLE_RATE, channels=CHANNELS, blocksize=FRAME_SIZE, callback=on_input
-        )
-
-        def on_output(out_data, frames, time_, status):
-            assert out_data.shape == (FRAME_SIZE, 1), out_data.shape
-            try:
-                pcm_data = output_queue.get(block=False)
-                # TODO: handle other shapes by using some form of fifo/ring buffer.
-                assert pcm_data.shape == (FRAME_SIZE,), pcm_data.shape
-                out_data[:, 0] = pcm_data
-            except queue.Empty:
-                out_data.fill(0)
-                log("[Skipped a frame!]")
-
-        out_stream = sd.OutputStream(
-            samplerate=SAMPLE_RATE,
-            channels=CHANNELS,
-            blocksize=FRAME_SIZE,
-            callback=on_output,
-        )
-
-        with in_stream, out_stream:
-            await asyncio.gather(recv_loop(), queue_loop(), decoder_loop())
+    with in_stream, out_stream:
+        await asyncio.gather(recv_loop(), queue_loop(), decoder_loop())
+    printer.log("info", "All done!")
 
 asyncio.run(main())
