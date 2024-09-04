9adfbe2f303ef5a688e0e1ea495b6d82967bce69
Alexandre Défossez <alexandre.defossez@gmail.com>
2024-09-04 22:04:50 +0200

    ok

--- client_opus.py
+++ client_opus.py
@@ -4,6 +4,7 @@
 
 import argparse
 import asyncio
+from dataclasses import dataclass
 import queue
 import sys
 
@@ -54,69 +55,159 @@ class RawPrinter:
         pass
 
 
+@dataclass
+class LineEntry:
+    msg: str
+    color: str | None = None
+
+    def render(self):
+        if self.color is None:
+            return self.msg
+        else:
+            return colorize(self.msg, self.color)
+
+    def __len__(self):
+        return len(self.msg)
+
+
+class Line:
+    def __init__(self, stream):
+        self.stream = stream
+        self._line: list[LineEntry] = []
+        self._has_padding: bool = False
+        self._max_line_length = 0
+
+    def __bool__(self):
+        return bool(self._line)
+
+    def __len__(self):
+        return sum(len(entry) for entry in self._line)
+
+    def add(self, msg: str, color: str | None = None) -> int:
+        entry = LineEntry(msg, color)
+        return self._add(entry)
+
+    def _add(self, entry: LineEntry) -> int:
+        if self._has_padding:
+            self.erase(count=0)
+        self._line.append(entry)
+        self.stream.write(entry.render())
+        self._max_line_length = max(self._max_line_length, len(self))
+        return len(entry)
+
+    def erase(self, count: int = 1):
+        if count:
+            entries = list(self._line[:-count])
+        else:
+            entries = list(self._line)
+        self._line.clear()
+        self.stream.write('\r')
+        for entry in entries:
+            self._line.append(entry)
+            self.stream.write(entry.render())
+
+        self._has_padding = False
+
+    def newline(self):
+        missing = self._max_line_length - len(self)
+        if missing > 0:
+            self.stream.write(' ' * missing)
+        self.stream.write('\n')
+        self._line.clear()
+        self._max_line_length = 0
+        self._has_padding = False
+
+    def flush(self):
+        missing = self._max_line_length - len(self)
+        if missing > 0:
+            self.stream.write(' ' * missing)
+            self._has_padding = True
+        self.stream.flush()
+
+
 class Printer:
     def __init__(self, max_cols: int = 80, stream=sys.stdout, err_stream=sys.stderr):
         self.max_cols = max_cols
-        self.current_line: str = ''
+        self.line = Line(stream)
         self.stream = stream
         self.err_stream = err_stream
         self._pending_count = 0
         self._pending_printed = False
 
     def print_header(self):
-        print('-' * self.max_cols)
+        self.line.add(' ' + '-' * (self.max_cols) + ' ')
+        self.line.newline()
+        self.line.flush()
+        self.line.add('| ')
 
     def _remove_pending(self) -> bool:
         if self._pending_printed:
             self._pending_printed = False
-            self.stream.write('\r' + self.current_line + ' ' * 2)
+            self.line.erase(1)
             return True
         return False
 
-    def print_token(self, token: str):
+    def print_token(self, token: str, color: str | None = None):
         self._remove_pending()
-        if len(self.current_line) + len(token) <= self.max_cols:
-            self.stream.write(token)
-            self.current_line += token
+        remaining = self.max_cols - len(self.line)
+        if len(token) <= remaining:
+            self.line.add(token, color)
         else:
+            end = ' ' * remaining + ' |'
             if token.startswith(' '):
                 token = token.lstrip()
-                self.stream.write('\n' + token)
-                self.current_line = token
+                self.line.add(end)
+                self.line.newline()
+                self.line.add('| ')
+                self.line.add(token, color)
             else:
-                if ' ' in self.current_line:
-                    line, word = self.current_line.rsplit(' ', 1)
-                    token = word.lstrip() + token
-                    sys.stdout.write('\r' + line + ' ' * (len(word) + 1) + '\n' + token)
-                    self.current_line = token
+                assert color is None
+                erase_count = None
+                cumulated = ''
+                for idx, entry in enumerate(self.line._line[::-1]):
+                    if entry.color:
+                        # probably a LAG message
+                        erase_count = idx
+                        break
+                    if entry.msg.startswith(' '):
+                        erase_count = idx + 1
+                        cumulated = entry.msg + cumulated
+                if erase_count is not None:
+                    if erase_count > 0:
+                        self.line.erase(erase_count)
+                    remaining = self.max_cols - len(self.line)
+                    end = ' ' * remaining + ' |'
+                    self.line.add(end)
+                    self.line.newline()
+                    self.line.add('| ')
+                    token = cumulated.lstrip() + token
+                    self.line.add(token)
                 else:
-                    remaining = self.max_cols - len(self.current_line)
-                    sys.stdout.write(token[:remaining] + '\n' + token[remaining:])
-                    self.current_line = token[remaining:]
-        self.stream.flush()
+                    self.line.add(token[:remaining])
+                    self.line.newline()
+                    self.line.add('| ')
+                    self.line.add(token[remaining:])
+        self.line.flush()
 
     def log(self, level: str, msg: str):
         msg = make_log(level, msg)
-        removed = self._remove_pending()
-        if self.current_line:
-            self.stream.write('\n')
-        elif removed:
-            self.stream.write('\t')
-        self.stream.flush()
+        self._remove_pending()
+        if self.line:
+            self.line.newline()
+        self.line.flush()
         print(msg, file=self.err_stream)
         self.err_stream.flush()
 
     def print_lag(self):
-        self.print_token(colorize(' [LAG]', '31'))
+        self.print_token(' [LAG]', '31')
 
     def print_pending(self):
         chars = ['|', '/', '-', '\\']
         count = int(self._pending_count / 5)
         char = chars[count % len(chars)]
         colors = ['32', '33', '31']
-        char = colorize(char, colors[count % len(colors)])
-        self.stream.write('\r' + self.current_line + ' ' + char)
-        self.stream.flush()
+        self._remove_pending()
+        self.line.add(char, colors[count % len(colors)])
         self._pending_printed = True
         self._pending_count += 1
 
@@ -251,10 +342,18 @@ def main():
     parser.add_argument("--port", default=8998, type=int)
     args = parser.parse_args()
     printer: AnyPrinter
+
     if sys.stdout.isatty():
         printer = Printer()
     else:
         printer = RawPrinter()
+    line = printer.line
+    for k in range(10):
+        printer.print_pending()
+        import time
+        time.sleep(0.05)
+    printer.print_token("Hello")
+    printer.print_token(" world")
     try:
         asyncio.run(run(printer, args))
     except KeyboardInterrupt:
