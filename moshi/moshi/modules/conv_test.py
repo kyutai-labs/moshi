ab5c45c75c2e969543bbcb1137fce89676206ad4
jerryli27 <jerrylijiaming@gmail.com>
2024-10-11 20:37:10 +0000

    Style changes to pass the precommits

--- moshi/moshi/modules/conv_test.py
+++ moshi/moshi/modules/conv_test.py
@@ -6,7 +6,7 @@ import pytest
 from .conv import StreamingConv1d, StreamingConvTranspose1d
 
 
-torch.backends.cudnn.enabled = False  # Disable cuDNN for deterministic behavior
+torch.backends.cudnn.enabled = False  # Disable cuDNN for deterministic behavior and for numerical stability
 
 
 CONV1D_DATA = [
@@ -49,111 +49,109 @@ CONV1D_TRANSPOSE_DATA = [
     ),
 ]
 
+
 def _init_weights(module, generator=None):
-  for name, param in module.named_parameters():
-    if "weight" in name:
-      nn.init.xavier_uniform_(param, generator=generator)
-    elif "bias" in name:
-      nn.init.constant_(param, 0.0)
-    else:
-        nn.init.xavier_uniform_(param, generator=generator)
-
-
-
-@pytest.mark.parametrize("batch_size,in_channels,out_channels,seq_len,kernel_size", CONV1D_DATA)
-def test_conv1d(batch_size,in_channels,out_channels,seq_len,kernel_size):
-  """Test that StreamingConv1d() calls are causal. Having new inputs does not change the previous output."""
-  assert seq_len > kernel_size
-
-  layer = StreamingConv1d(in_channels, out_channels, kernel_size, causal=True, norm="none", pad_mode="constant")
-
-
-  generator = torch.Generator()
-  generator = generator.manual_seed(41)
-  layer.apply(functools.partial(_init_weights, generator=generator))
-
-  shape = (batch_size, in_channels, seq_len,)
-  input_hidden_states = torch.rand(shape)
-
-  expected_output = layer(input_hidden_states)
-
-  for end_index in range(kernel_size, seq_len+1):
-      actual_output = layer(input_hidden_states[..., :end_index])
-      torch.testing.assert_close(actual_output, expected_output[..., :actual_output.shape[-1]],
-                                 msg=lambda original_msg: f"Failed at end_index={end_index}: \n{original_msg}")
+    for name, param in module.named_parameters():
+        if "weight" in name:
+            nn.init.xavier_uniform_(param, generator=generator)
+        elif "bias" in name:
+            nn.init.constant_(param, 0.0)
+        else:
+            nn.init.xavier_uniform_(param, generator=generator)
 
 
 @pytest.mark.parametrize("batch_size,in_channels,out_channels,seq_len,kernel_size", CONV1D_DATA)
-def test_conv1d_streaming(batch_size,in_channels,out_channels,seq_len,kernel_size):
-  """Test that StreamingConv1d() streaming works as expected."""
-  assert seq_len > kernel_size
+def test_conv1d(batch_size, in_channels, out_channels, seq_len, kernel_size):
+    """Test that StreamingConv1d() calls are causal. Having new inputs does not change the previous output."""
+    assert seq_len > kernel_size
 
-  layer = StreamingConv1d(in_channels, out_channels, kernel_size, causal=True, norm="none", pad_mode="constant")
+    layer = StreamingConv1d(in_channels, out_channels, kernel_size, causal=True, norm="none", pad_mode="constant")
 
+    generator = torch.Generator()
+    generator = generator.manual_seed(41)
+    layer.apply(functools.partial(_init_weights, generator=generator))
 
-  generator = torch.Generator()
-  generator = generator.manual_seed(41)
-  layer.apply(functools.partial(_init_weights, generator=generator))
+    shape = (batch_size, in_channels, seq_len,)
+    input_hidden_states = torch.rand(shape)
 
-  shape = (batch_size, in_channels, seq_len,)
-  input_hidden_states = torch.rand(shape)
-  expected_output = layer(input_hidden_states)
+    expected_output = layer(input_hidden_states)
 
-  start_index = 0
-  actual_outputs = []
-  with layer.streaming(batch_size=batch_size):
-    for end_index in range(kernel_size, seq_len+1):
-      actual_output = layer(input_hidden_states[..., start_index:end_index])
-      start_index = end_index
-      actual_outputs.append(actual_output)
-  actual_outputs = torch.concat(actual_outputs, axis=-1)
-
-  torch.testing.assert_close(actual_outputs, expected_output)
+    for end_index in range(kernel_size, seq_len + 1):
+        actual_output = layer(input_hidden_states[..., :end_index])
+        torch.testing.assert_close(actual_output, expected_output[..., :actual_output.shape[-1]],
+                                   msg=lambda original_msg: f"Failed at end_index={end_index}: \n{original_msg}")
 
 
-@pytest.mark.parametrize("batch_size,in_channels,out_channels,seq_len,kernel_size,stride", CONV1D_TRANSPOSE_DATA)
-def test_conv1d_transpose(batch_size,in_channels,out_channels,seq_len,kernel_size,stride):
-  """Test that StreamingConvTranspose1d() calls are causal. Having new inputs does not change the previous output."""
-  assert seq_len > kernel_size
+@pytest.mark.parametrize("batch_size, in_channels, out_channels, seq_len, kernel_size", CONV1D_DATA)
+def test_conv1d_streaming(batch_size, in_channels, out_channels, seq_len, kernel_size):
+    """Test that StreamingConv1d() streaming works as expected."""
+    assert seq_len > kernel_size
 
-  layer = StreamingConvTranspose1d(in_channels, out_channels, kernel_size,stride, causal=True, norm="none")
+    layer = StreamingConv1d(in_channels, out_channels, kernel_size, causal=True, norm="none", pad_mode="constant")
 
-  generator = torch.Generator()
-  generator = generator.manual_seed(41)
-  layer.apply(functools.partial(_init_weights, generator=generator))
+    generator = torch.Generator()
+    generator = generator.manual_seed(41)
+    layer.apply(functools.partial(_init_weights, generator=generator))
 
-  shape = (batch_size, in_channels, seq_len,)
-  input_hidden_states = torch.rand(shape)
-  expected_output = layer(input_hidden_states)
+    shape = (batch_size, in_channels, seq_len,)
+    input_hidden_states = torch.rand(shape)
+    expected_output = layer(input_hidden_states)
 
-  for end_index in range(kernel_size, seq_len+1):
-      actual_output = layer(input_hidden_states[..., :end_index])
-      torch.testing.assert_close(actual_output, expected_output[..., :actual_output.shape[-1]],
-                                 msg=lambda original_msg: f"Failed at end_index={end_index}: \n{original_msg}")
+    start_index = 0
+    actual_outputs = []
+    with layer.streaming(batch_size=batch_size):
+        for end_index in range(kernel_size, seq_len + 1):
+            actual_output = layer(input_hidden_states[..., start_index:end_index])
+            start_index = end_index
+            actual_outputs.append(actual_output)
+    actual_outputs = torch.cat(actual_outputs, dim=-1)
+
+    torch.testing.assert_close(actual_outputs, expected_output)
 
 
-@pytest.mark.parametrize("batch_size,in_channels,out_channels,seq_len,kernel_size,stride", CONV1D_TRANSPOSE_DATA)
-def test_conv1d_transpose_streaming(batch_size,in_channels,out_channels,seq_len,kernel_size,stride):
-  """Test that StreamingConvTranspose1d() streaming works as expected."""
-  assert seq_len > kernel_size
+@pytest.mark.parametrize("batch_size, in_channels, out_channels, seq_len, kernel_size, stride", CONV1D_TRANSPOSE_DATA)
+def test_conv1d_transpose(batch_size, in_channels, out_channels, seq_len, kernel_size, stride):
+    """Test that StreamingConvTranspose1d() calls are causal. Having new inputs does not change the previous output."""
+    assert seq_len > kernel_size
 
-  layer = StreamingConvTranspose1d(in_channels, out_channels, kernel_size,stride, causal=True, norm="none")
+    layer = StreamingConvTranspose1d(in_channels, out_channels, kernel_size, stride, causal=True, norm="none")
 
-  generator = torch.Generator()
-  generator = generator.manual_seed(41)
-  layer.apply(functools.partial(_init_weights, generator=generator))
+    generator = torch.Generator()
+    generator = generator.manual_seed(41)
+    layer.apply(functools.partial(_init_weights, generator=generator))
 
-  shape = (batch_size, in_channels, seq_len,)
-  input_hidden_states = torch.rand(shape)
-  expected_output = layer(input_hidden_states)
+    shape = (batch_size, in_channels, seq_len,)
+    input_hidden_states = torch.rand(shape)
+    expected_output = layer(input_hidden_states)
 
-  start_index = 0
-  actual_outputs = []
-  with layer.streaming(batch_size=batch_size):
-    for end_index in range(kernel_size, seq_len+1):
-      actual_output = layer(input_hidden_states[..., start_index:end_index])
-      start_index = end_index
-      actual_outputs.append(actual_output)
-  actual_outputs = torch.concat(actual_outputs, axis=-1)
+    for end_index in range(kernel_size, seq_len + 1):
+        actual_output = layer(input_hidden_states[..., :end_index])
+        torch.testing.assert_close(actual_output, expected_output[..., :actual_output.shape[-1]],
+                                   msg=lambda original_msg: f"Failed at end_index={end_index}: \n{original_msg}")
 
-  torch.testing.assert_close(actual_outputs, expected_output)
+
+@pytest.mark.parametrize("batch_size, in_channels, out_channels, seq_len, kernel_size, stride", CONV1D_TRANSPOSE_DATA)
+def test_conv1d_transpose_streaming(batch_size, in_channels, out_channels, seq_len, kernel_size, stride):
+    """Test that StreamingConvTranspose1d() streaming works as expected."""
+    assert seq_len > kernel_size
+
+    layer = StreamingConvTranspose1d(in_channels, out_channels, kernel_size, stride, causal=True, norm="none")
+
+    generator = torch.Generator()
+    generator = generator.manual_seed(41)
+    layer.apply(functools.partial(_init_weights, generator=generator))
+
+    shape = (batch_size, in_channels, seq_len,)
+    input_hidden_states = torch.rand(shape)
+    expected_output = layer(input_hidden_states)
+
+    start_index = 0
+    actual_outputs = []
+    with layer.streaming(batch_size=batch_size):
+        for end_index in range(kernel_size, seq_len + 1):
+            actual_output = layer(input_hidden_states[..., start_index:end_index])
+            start_index = end_index
+            actual_outputs.append(actual_output)
+    actual_outputs = torch.cat(actual_outputs, dim=-1)
+
+    torch.testing.assert_close(actual_outputs, expected_output)
--- moshi/moshi/modules/seanet_test.py
+++ moshi/moshi/modules/seanet_test.py
@@ -6,7 +6,7 @@ import pytest
 from .seanet import SEANetResnetBlock, SEANetDecoder
 
 
-torch.backends.cudnn.enabled = False  # Disable cuDNN for deterministic behavior
+torch.backends.cudnn.enabled = False  # Disable cuDNN for deterministic behavior and for numerical stability
 
 
 SEANET_RESNET_DATA = [
@@ -99,92 +99,89 @@ SEANET_KWARGS_DATA = [
 
 
 def _init_weights(module, generator=None):
-  for name, param in module.named_parameters():
-    if "weight" in name:
-      nn.init.xavier_uniform_(param, generator=generator)
-    elif "bias" in name:
-      nn.init.constant_(param, 0.0)
-    else:
-        nn.init.xavier_uniform_(param, generator=generator)
+    for name, param in module.named_parameters():
+        if "weight" in name:
+            nn.init.xavier_uniform_(param, generator=generator)
+        elif "bias" in name:
+            nn.init.constant_(param, 0.0)
+        else:
+            nn.init.xavier_uniform_(param, generator=generator)
 
 
+@pytest.mark.parametrize("batch_size, dim, res_layer_index, seq_len, kernel_size", SEANET_RESNET_DATA)
+def test_resnet(batch_size, dim, res_layer_index, seq_len, kernel_size):
+    """Test that SEANetResnetBlock() calls are causal. Having new inputs does not change the previous output."""
+    assert seq_len > kernel_size
+
+    dilation_base = 2
+    layer = SEANetResnetBlock(dim=dim, dilations=[dilation_base**res_layer_index, 1], pad_mode="constant", causal=True)
+
+    generator = torch.Generator()
+    generator = generator.manual_seed(41)
+    layer.apply(functools.partial(_init_weights, generator=generator))
+
+    shape = (batch_size, dim, seq_len,)
+    input_hidden_states = torch.rand(shape)
+
+    expected_output = layer(input_hidden_states)
+
+    for end_index in range(kernel_size, seq_len + 1):
+        actual_output = layer(input_hidden_states[..., :end_index])
+        torch.testing.assert_close(actual_output, expected_output[..., :actual_output.shape[-1]],
+                                   msg=lambda original_msg: f"Failed at end_index={end_index}: \n{original_msg}")
 
 
-@pytest.mark.parametrize("batch_size,dim,res_layer_index,seq_len,kernel_size", SEANET_RESNET_DATA)
-def test_resnet(batch_size,dim,res_layer_index,seq_len,kernel_size):
-  """Test that SEANetResnetBlock() calls are causal. Having new inputs does not change the previous output."""
-  assert seq_len > kernel_size
+@pytest.mark.parametrize("batch_size, dim, res_layer_index, seq_len, kernel_size", SEANET_RESNET_DATA)
+def test_resnet_streaming(batch_size, dim, res_layer_index, seq_len, kernel_size):
+    """Test that SEANetResnetBlock() streaming works as expected."""
+    assert seq_len > kernel_size
 
-  dilation_base = 2
-  layer = SEANetResnetBlock(dim=dim, dilations=[dilation_base**res_layer_index, 1], pad_mode="constant", causal=True)
+    dilation_base = 2
+    layer = SEANetResnetBlock(dim=dim, dilations=[dilation_base**res_layer_index, 1], pad_mode="constant", causal=True)
 
+    generator = torch.Generator()
+    generator = generator.manual_seed(41)
+    layer.apply(functools.partial(_init_weights, generator=generator))
 
-  generator = torch.Generator()
-  generator = generator.manual_seed(41)
-  layer.apply(functools.partial(_init_weights, generator=generator))
+    shape = (batch_size, dim, seq_len,)
+    input_hidden_states = torch.rand(shape)
 
-  shape = (batch_size, dim, seq_len,)
-  input_hidden_states = torch.rand(shape)
+    expected_output = layer(input_hidden_states)
 
-  expected_output = layer(input_hidden_states)
+    start_index = 0
+    actual_outputs = []
+    with layer.streaming(batch_size=batch_size):
+        for end_index in range(kernel_size, seq_len + 1):
+            actual_output = layer(input_hidden_states[..., start_index:end_index])
+            start_index = end_index
+            actual_outputs.append(actual_output)
+    actual_outputs = torch.cat(actual_outputs, dim=-1)
 
-  for end_index in range(kernel_size, seq_len+1):
-      actual_output = layer(input_hidden_states[..., :end_index])
-      torch.testing.assert_close(actual_output, expected_output[..., :actual_output.shape[-1]], msg=lambda original_msg: f"Failed at end_index={end_index}: \n{original_msg}")
-
-
-
-@pytest.mark.parametrize("batch_size,dim,res_layer_index,seq_len,kernel_size", SEANET_RESNET_DATA)
-def test_resnet_streaming(batch_size,dim,res_layer_index,seq_len,kernel_size):
-  """Test that SEANetResnetBlock() streaming works as expected."""
-  assert seq_len > kernel_size
-
-  dilation_base = 2
-  layer = SEANetResnetBlock(dim=dim, dilations=[dilation_base**res_layer_index, 1], pad_mode="constant", causal=True)
-
-
-  generator = torch.Generator()
-  generator = generator.manual_seed(41)
-  layer.apply(functools.partial(_init_weights, generator=generator))
-
-  shape = (batch_size, dim, seq_len,)
-  input_hidden_states = torch.rand(shape)
-
-  expected_output = layer(input_hidden_states)
-
-  start_index = 0
-  actual_outputs = []
-  with layer.streaming(batch_size=batch_size):
-    for end_index in range(kernel_size, seq_len+1):
-      actual_output = layer(input_hidden_states[..., start_index:end_index])
-      start_index = end_index
-      actual_outputs.append(actual_output)
-  actual_outputs = torch.concat(actual_outputs, axis=-1)
-
-  torch.testing.assert_close(actual_outputs, expected_output)
+    torch.testing.assert_close(actual_outputs, expected_output)
 
 
 @pytest.mark.parametrize("num_timesteps", NUM_TIMESTEPS_DATA)
 @pytest.mark.parametrize("seanet_kwargs", SEANET_KWARGS_DATA)
 def test_nonstreaming_causal_decode(num_timesteps, seanet_kwargs):
-  """Test that the SEANetDecoder does not depend on future inputs."""
+    """Test that the SEANetDecoder does not depend on future inputs."""
 
-  device = 'cuda' if torch.cuda.is_available() else 'cpu'
-  decoder = SEANetDecoder(**seanet_kwargs).to(device=device)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    decoder = SEANetDecoder(**seanet_kwargs).to(device=device)
 
-  generator = torch.Generator(device=device)
-  generator = generator.manual_seed(41)
-  decoder.apply(functools.partial(_init_weights, generator=generator))
+    generator = torch.Generator(device=device)
+    generator = generator.manual_seed(41)
+    decoder.apply(functools.partial(_init_weights, generator=generator))
 
-  rand_generator = torch.Generator(device=device)
-  rand_generator.manual_seed(2147483647)
-  with torch.no_grad():
-    codes = torch.randn(1, seanet_kwargs['dimension'], num_timesteps, generator=rand_generator, device=device)  # [B, K = 8, T]
-    expected_decoded = decoder(codes)
+    rand_generator = torch.Generator(device=device)
+    rand_generator.manual_seed(2147483647)
+    with torch.no_grad():
+        # [B, K = 8, T]
+        codes = torch.randn(1, seanet_kwargs['dimension'], num_timesteps, generator=rand_generator, device=device)
+        expected_decoded = decoder(codes)
 
-    num_timesteps = codes.shape[-1]
-    for t in range(num_timesteps):
-      current_codes = codes[..., :t+1]
-      actual_decoded = decoder(current_codes)
-      torch.testing.assert_close(expected_decoded[..., :actual_decoded.shape[-1]], actual_decoded,
-                                 msg=lambda original_msg: f"Failed at t={t}: \n{original_msg}")
+        num_timesteps = codes.shape[-1]
+        for t in range(num_timesteps):
+            current_codes = codes[..., :t + 1]
+            actual_decoded = decoder(current_codes)
+            torch.testing.assert_close(expected_decoded[..., :actual_decoded.shape[-1]], actual_decoded,
+                                       msg=lambda original_msg: f"Failed at t={t}: \n{original_msg}")
