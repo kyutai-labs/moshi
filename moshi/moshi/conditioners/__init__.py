45371d8c9d552750369584616a13ea2074e2e12c
laurent <laurent.mazare@gmail.com>
2025-02-01 19:31:40 +0100

    Fix lint issue.

--- moshi/moshi/conditioners/__init__.py
+++ moshi/moshi/conditioners/__init__.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 # Copyright (c) Kyutai, all rights reserved.
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
