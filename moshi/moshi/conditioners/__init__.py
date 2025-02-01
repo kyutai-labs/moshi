a6a54810a7c64904b620b527db193d69d26c1104
laurent <laurent.mazare@gmail.com>
2025-02-01 19:30:43 +0100

    Add the conditioners bits.

---
+++ moshi/moshi/conditioners/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Kyutai, all rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Modules to help doing generations under some fixed conditions.
+"""
+
+from .base import ConditionProvider
