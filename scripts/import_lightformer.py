d5aaa8f32fb4d82c5abd08f68afffd8e71bdde8d
Alexandre Défossez <alexandre.defossez@gmail.com>
2025-02-05 16:29:59 +0100

    fixes

--- scripts/import_lightformer.py
+++ scripts/import_lightformer.py
@@ -28,17 +28,16 @@ def import_model(
         schedule = list(range(in_n_q))
 
     num_weights = max(schedule) + 1
-    schedule = schedule[:out_n_q]
     kept_weights = max(schedule) + 1
 
     for idx in range(cfg.transformer_lm.depformer_num_layers):
         in_proj_key = f"depformer.layers.{idx}.self_attn.in_proj_weight"
         in_proj = model[in_proj_key]
-        in_proj = in_proj.view(num_weights, *in_proj.shape[1:])
+        in_proj = in_proj.view(num_weights, -1, *in_proj.shape[1:])
         model[in_proj_key] = in_proj[:kept_weights].view(-1, *in_proj.shape[2:]).contiguous()
         out_proj_key = f"depformer.layers.{idx}.self_attn.out_proj.weight"
         out_proj = model[out_proj_key]
-        out_proj = out_proj.view(num_weights, *out_proj.shape[1:])
+        out_proj = out_proj.view(num_weights, -1, *out_proj.shape[1:])
         model[in_proj_key] = in_proj[:kept_weights].view(-1, *in_proj.shape[2:]).contiguous()
         model[out_proj_key] = out_proj[:kept_weights].view(-1, *out_proj.shape[2:]).contiguous()
         model[out_proj_key] = out_proj[: out_proj.shape[0] // 2]
@@ -48,11 +47,13 @@ def import_model(
         del model[f"depformer_emb.{dep_idx}.weight"]
     for dep_idx in range(out_n_q, in_n_q):
         del model[f"linears.{dep_idx}.weight"]
-        real_idx = schedule[dep_idx]
-        model.pop(f"depformer_in.{real_idx}.weight", None)
+    for real_idx in range(kept_weights, num_weights):
+        model.pop(f"depformer_in.{real_idx}.weight")
         for idx in range(DEPFORMER_LAYERS):
-            model.pop(f"depformer.layers.{idx}.gating.{real_idx}.linear_in.weight", None)
-            model.pop(f"depformer.layers.{idx}.gating.{real_idx}.linear_out.weight", None)
+            model.pop(f"depformer.layers.{idx}.gating.{real_idx}.linear_in.weight")
+            model.pop(f"depformer.layers.{idx}.gating.{real_idx}.linear_out.weight")
+
+    schedule = schedule[:out_n_q]
 
     save_file(model, out_path)
 
