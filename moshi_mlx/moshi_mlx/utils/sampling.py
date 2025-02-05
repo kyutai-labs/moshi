f3459033631c023c081f2b1cb5ba50990a527016
Laurent Mazare <laurent.mazare@gmail.com>
2025-02-05 12:21:42 +0100

    Add topk sampling to the MLX implementation. (#210)

--- moshi_mlx/moshi_mlx/utils/sampling.py
+++ moshi_mlx/moshi_mlx/utils/sampling.py
@@ -66,6 +66,32 @@ def min_p_sampling(
 
 
 @partial(mx.compile, inputs=mx.random.state, outputs=mx.random.state)
+def top_k_sampling(
+    logprobs: mx.array,
+    top_k: int,
+    temperature=1.0,
+) -> mx.array:
+    """
+    Sample from only the top K tokens ranked by probability.
+    Args:
+        logprobs: A vector of log probabilities.
+        top_k (int): Top k tokens to sample from.
+    """
+    vocab_size = logprobs.shape[-1]
+    if not isinstance(top_k, int) or not (0 < top_k < vocab_size):
+        raise ValueError(
+            f"`top_k` has to be an integer in the (0, {vocab_size}] interval,"
+            f" but is {top_k}."
+        )
+    logprobs = logprobs * (1 / temperature)
+    mask_idx = mx.argpartition(-logprobs, kth=top_k - 1, axis=-1)[..., top_k:]
+    masked_logprobs = mx.put_along_axis(
+        logprobs, mask_idx, mx.array(-float("inf"), logprobs.dtype), axis=-1
+    )
+    return mx.random.categorical(masked_logprobs, axis=-1)
+
+
+@partial(mx.compile, inputs=mx.random.state, outputs=mx.random.state)
 def top_p_sampling(logits: mx.array, top_p: float, temperature: float) -> mx.array:
     """
     Apply top-p (nucleus) sampling to logits.
@@ -108,6 +134,7 @@ def categorical_sampling(logits, temp):
 class Sampler:
     temp: float = 0.8
     top_p: float = 0.95
+    top_k: int | None = None
     min_p: float = 0.0
     min_tokens_to_keep: int = 1
     logit_bias: dict[int, float] | None = None
@@ -122,7 +149,9 @@ class Sampler:
         if self.temp == 0:
             token = mx.argmax(logits, axis=-1)
         else:
-            if self.top_p > 0 and self.top_p < 1.0:
+            if self.top_k is not None and self.top_k > 0:
+                token = top_k_sampling(logits, self.top_k, self.temp)
+            elif self.top_p > 0 and self.top_p < 1.0:
                 token = top_p_sampling(logits, self.top_p, self.temp)
             elif self.min_p != 0.0:
                 token = min_p_sampling(
