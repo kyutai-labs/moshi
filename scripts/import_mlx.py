9f64c092cfcadc42ce1fe79e07daf80d026b0321
laurent <laurent.mazare@gmail.com>
2025-02-05 19:38:09 +0100

    Weights-per-step export.

--- scripts/import_mlx.py
+++ scripts/import_mlx.py
@@ -3,6 +3,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import argparse
+
 import torch
 from pathlib import Path
 from safetensors.torch import save_file, load_file
@@ -29,7 +30,7 @@ def import_model(
             break
     out_n_q: int | None = None
     for idx in range(999):
-        name = f"depformer_in.{idx}.weight"
+        name = f"linears.{idx}.weight"
         if name not in tch_model:
             out_n_q = idx
             break
@@ -37,6 +38,10 @@ def import_model(
     assert out_n_q is not None
     if not silent:
         print(f"in_n_q: {in_n_q}, out_n_q: {out_n_q}")
+
+    if weights_per_step_schedule is not None:
+        if len(weights_per_step_schedule) != out_n_q:
+            raise ValueError("inconsistent weights_per_step_schedule", weights_per_step_schedule, out_n_q)
 
     depformer_layers: int | None = None
     for idx in range(999):
@@ -60,7 +65,7 @@ def import_model(
         model[dst_name] = tch_model[src_name]
 
     for k, v in sorted(tch_model.items()):
-        # print(k, v.shape, v.dtype)
+        print(k, v.shape, v.dtype)
         if k.startswith("transformer"):
             if k.endswith(".alpha"):
                 v = v[0, 0]
@@ -68,36 +73,42 @@ def import_model(
             k = k.replace(".in_proj_weight", ".in_proj.weight")
             model[k] = v
 
-    # Only export the first 8 slices of the depformer (main).
+    # Only export the first slices of the depformer (main).
     if max_out_n_q is not None:
         exported_out_n_q = min(max_out_n_q, out_n_q)
         print(f"only exporting the first {exported_out_n_q} depformer layers")
     else:
         exported_out_n_q = out_n_q
 
-    if weights_per_step_schedule is None:
-        weights_per_step_schedule = list(range(exported_out_n_q))
+    for idx in range(exported_out_n_q):
+        if weights_per_step_schedule is not None:
+            tch_idx = weights_per_step_schedule[idx]
+        else:
+            tch_idx = idx
 
-    for idx, weight_idx in enumerate(weights_per_step_schedule):
         base = f"depformer.slices.{idx}."
-        model[base + "linear_in.weight"] = tch_model[f"depformer_in.{idx}.weight"]
+        model[base + "linear_in.weight"] = tch_model[f"depformer_in.{tch_idx}.weight"]
         model[base + "linear_out.weight"] = tch_model[f"linears.{idx}.weight"]
         if idx == 0:
             model[base + "emb.weight"] = tch_model["depformer_text_emb.weight"]
+            if "depformer_text_emb.low_rank.weight" in tch_model:
+                model[base + "emb.low_rank.weight"] = tch_model["depformer_text_emb.low_rank.weight"]
         else:
-            model[base + "emb.weight"] = tch_model[f"depformer_emb.{idx-1}.weight"]
+            model[base + "emb.weight"] = tch_model[f"depformer_emb.{tch_idx-1}.weight"]
+            if f"depformer_emb.{tch_idx-1}.low_rank.weight" in tch_model:
+                model[base + "emb.low_rank.weight"] = tch_model[f"depformer_emb.{tch_idx-1}.low_rank.weight"]
 
         for layer_idx in range(depformer_layers):
             layer = base + f"transformer.layers.{layer_idx}."
             # WARNING: note that this uses in_proj_weight vs out_proj.weight
             model[layer + "self_attn.in_proj.weight"] = (
                 tch_model[f"depformer.layers.{layer_idx}.self_attn.in_proj_weight"]
-                .chunk(out_n_q)[idx]
+                .chunk(out_n_q)[tch_idx]
                 .clone()
             )
             model[layer + "self_attn.out_proj.weight"] = (
                 tch_model[f"depformer.layers.{layer_idx}.self_attn.out_proj.weight"]
-                .chunk(out_n_q)[idx]
+                .chunk(out_n_q)[tch_idx]
                 .clone()
             )
             model[layer + "norm1.weight"] = tch_model[
@@ -107,10 +118,10 @@ def import_model(
                 f"depformer.layers.{layer_idx}.norm2.alpha"
             ][0, 0].clone()
             model[layer + "gating.linear_in.weight"] = tch_model[
-                f"depformer.layers.{layer_idx}.gating.{idx}.linear_in.weight"
+                f"depformer.layers.{layer_idx}.gating.{tch_idx}.linear_in.weight"
             ]
             model[layer + "gating.linear_out.weight"] = tch_model[
-                f"depformer.layers.{layer_idx}.gating.{idx}.linear_out.weight"
+                f"depformer.layers.{layer_idx}.gating.{tch_idx}.linear_out.weight"
             ]
 
     save_file(model, out_path)
@@ -123,6 +134,7 @@ def main():
     parser.add_argument(
         "-s", "--silent", action="store_true", help="only prints the checkpoint name"
     )
+    parser.add_argument("--wpss", type=str, help="weights per step schedule config")
     parser.add_argument(
         "--max-out-n-q",
         type=int,
@@ -130,11 +142,20 @@ def main():
     )
     args = parser.parse_args()
 
+    wpss = None
+    if args.wpss is not None:
+        if args.wpss == "hibiki-2b":
+            wpss = [0, 1, 2, 4, 5, 6, 7, 8] + [9] * 23
+
     ckpt_path = Path(args.checkpoint)
     out_path = Path(args.out)
     if not out_path.exists():
         import_model(
-            ckpt_path, out_path, silent=args.silent, max_out_n_q=args.max_out_n_q
+            ckpt_path,
+            out_path,
+            weights_per_step_schedule=wpss,
+            silent=args.silent,
+            max_out_n_q=args.max_out_n_q
         )
     print(out_path)
 
