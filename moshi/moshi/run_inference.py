15923b8051ffc895eff4c76addff59090b33d386
Alexandre Défossez <alexandre.defossez@gmail.com>
2025-02-06 11:06:40 +0100

    adding text output on run inference

--- moshi/moshi/run_inference.py
+++ moshi/moshi/run_inference.py
@@ -7,6 +7,7 @@ from collections import deque
 from dataclasses import dataclass
 from pathlib import Path
 import random
+import sys
 import time
 
 import numpy as np
@@ -15,7 +16,7 @@ import torch
 import sphn
 
 
-from .client_utils import log
+from .client_utils import log, AnyPrinter, Printer, RawPrinter
 from .conditioners import ConditionAttributes, ConditionTensors
 from .models import loaders, MimiModel, LMModel, LMGen
 
@@ -66,6 +67,11 @@ class InferenceState:
         self.batch_size = batch_size
         self.mimi.streaming_forever(batch_size)
         self.lm_gen.streaming_forever(batch_size)
+        self.printer: AnyPrinter
+        if sys.stdout.isatty():
+            self.printer = Printer()
+        else:
+            self.printer = RawPrinter()
 
     def run(self, in_pcms: torch.Tensor) -> list[tuple[torch.Tensor, torch.Tensor]]:
         """Returns a list of tupel `(text_tokens, audio_tokens)`"""
@@ -76,7 +82,7 @@ class InferenceState:
         # for the EOS on the output text stream to be emitted, as indication that the model is done.
         eos_reached: list[bool] = [False] * self.batch_size
         need_eos_input: bool = True
-        log("info", "starting the inference loop")
+        self.printer.log("info", "starting the inference loop")
         device = self.lm_gen.lm_model.device
         start_time = time.time()
         ntokens = 0
@@ -85,6 +91,7 @@ class InferenceState:
         chunks = deque([
             chunk for chunk in in_pcms.split(self.frame_size, dim=2)
             if chunk.shape[-1] == self.frame_size])
+        self.printer.print_header()
         while not all(eos_reached):
             if chunks:
                 chunk = chunks.popleft()
@@ -124,9 +131,14 @@ class InferenceState:
 
                 out_text_tokens_per_item[b].append(one_text)
                 out_pcms_per_item[b].append(one_pcm)
+                if b == 0:
+                    if one_text.item() not in [0, 3]:
+                        text = self.text_tokenizer.id_to_piece(one_text.item())  # pyright: ignore
+                        text = text.replace("▁", " ")
+                        self.printer.print_token(text)
             ntokens += 1
         dt = time.time() - start_time
-        log("info", f"processed {ntokens} steps in {dt:.0f}s, {1000 * dt / ntokens:.2f}ms/step")
+        self.printer.log("info", f"processed {ntokens} steps in {dt:.0f}s, {1000 * dt / ntokens:.2f}ms/step")
         out = [
             (torch.cat(one_texts, dim=0), torch.cat(one_pcms, dim=1))
             for one_texts, one_pcms in zip(out_text_tokens_per_item, out_pcms_per_item)
