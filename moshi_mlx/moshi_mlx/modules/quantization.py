b092d113bfdce77f3f37681899a4783e7c7e35b7
Laurent Mazare <laurent.mazare@gmail.com>
2025-02-22 08:52:41 +0000

    Add an MLX implementation for mimi (#230)
    * Start adding mimi to the mlx codebase.
    
    * Finish the conv module.
    
    * Start adding the seanet module.
    
    * More seanet.
    
    * Add to seanet.
    
    * Start adding some quantization.
    
    * More quantization support.
    
    * Formatting.
    
    * Make pyright happy.
    
    * Add the mimi config.
    
    * More mimi implementation.
    
    * Add a test script.
    
    * Shape bugfixes.
    
    * Bugfixes...
    
    * Again more mimi.
    
    * Get some roundtrip to work.
    
    * Improve the weight loading.
    
    * Hacky import.
    
    * Get the weight loading to work.
    
    * Write the generated file.
    
    * Rename the hacky weight loader.
    
    * Fix the pre-commit issue.

---
+++ moshi_mlx/moshi_mlx/modules/quantization.py
@@ -0,0 +1,177 @@
+# Copyright (c) Kyutai, all rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .conv import Conv1d
+
+import mlx.core as mx
+import mlx.nn as nn
+
+
+class EuclideanCodebook(nn.Module):
+    def __init__(self, dim: int, codebook_size: int):
+        super().__init__()
+        self._epsilon = 1e-5
+        self._dim = dim
+        self.initialized = mx.zeros([1], dtype=mx.float32)
+        self.embedding_sum = mx.zeros([codebook_size, dim], dtype=mx.float32)
+        self.cluster_usage = mx.zeros([codebook_size], dtype=mx.float32)
+        cluster_usage = mx.maximum(self.cluster_usage, self._epsilon)[:, None]
+        self._embedding = self.embedding_sum / cluster_usage
+        self._c2 = self._embedding.square().sum(axis=-1) / 2
+
+    def update(self, parameters: dict) -> nn.Module:
+        super().update(parameters)
+        cluster_usage = mx.maximum(self.cluster_usage, self._epsilon)[:, None]
+        self._embedding = self.embedding_sum / cluster_usage
+        self._c2 = self._embedding.square().sum(axis=-1) / 2
+        return self
+
+    def encode(self, xs: mx.array) -> mx.array:
+        target_shape = xs.shape[:-1]
+        xs = xs.flatten(end_axis=-2)
+        dot_prod = xs @ self._embedding.swapaxes(-1, -2)
+        return (self._c2 - dot_prod).argmin(axis=-1).reshape(target_shape)
+
+    def decode(self, xs: mx.array) -> mx.array:
+        target_shape = list(xs.shape) + [self._dim]
+        return mx.take(self._embedding, xs.flatten(), axis=0).reshape(target_shape)
+
+
+class VectorQuantization(nn.Module):
+    def __init__(self, dim: int, codebook_size: int, codebook_dim: int | None):
+        super().__init__()
+        codebook_dim = dim if codebook_dim is None else codebook_dim
+        if dim == codebook_dim:
+            self.project_in = None
+            self.project_out = None
+        else:
+            self.project_in = nn.Linear(dim, codebook_dim)
+            self.project_out = nn.Linear(codebook_dim, dim)
+        self.codebook = EuclideanCodebook(dim=codebook_dim, codebook_size=codebook_size)
+
+    def encode(self, xs: mx.array) -> mx.array:
+        xs = xs.swapaxes(-1, -2)
+        if self.project_in is not None:
+            xs = self.project_in(xs)
+        return self.codebook.encode(xs)
+
+    def decode(self, xs: mx.array) -> mx.array:
+        xs = self.codebook.decode(xs)
+        if self.project_out is not None:
+            xs = self.project_out(xs)
+        return xs.swapaxes(-1, -2)
+
+
+class ResidualVectorQuantization(nn.Module):
+    def __init__(self, nq: int, dim: int, codebook_size: int, codebook_dim: int | None):
+        super().__init__()
+        layers = []
+        for _ in range(nq):
+            vq = VectorQuantization(
+                dim=dim,
+                codebook_size=codebook_size,
+                codebook_dim=codebook_dim,
+            )
+            layers.append(vq)
+        self.layers = layers
+
+    def encode(self, xs: mx.array) -> mx.array:
+        codes = []
+        residual = xs
+        for layer in self.layers:
+            indices = layer.encode(residual)
+            quantized = layer.decode(indices)
+            residual = residual - quantized
+            codes.append(indices)
+        return mx.stack(codes, axis=0)
+
+    def decode(self, xs: mx.array) -> mx.array:
+        seq_len = xs.shape[0]
+        quantized = self.layers[0].decode(xs[0])
+        for i in range(1, seq_len):
+            quantized = quantized + self.layers[i].decode(xs[i])
+        return quantized
+
+
+class ResidualVectorQuantizer(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        input_dim: int | None,
+        output_dim: int | None,
+        nq: int,
+        bins: int,
+        force_projection: bool,
+    ):
+        super().__init__()
+        input_dim = dim if input_dim is None else input_dim
+        output_dim = dim if output_dim is None else output_dim
+        if input_dim == dim and not force_projection:
+            self.input_proj = None
+        else:
+            self.input_proj = Conv1d(input_dim, dim, 1, bias=False)
+        if output_dim == dim and not force_projection:
+            self.output_proj = None
+        else:
+            self.output_proj = Conv1d(dim, output_dim, 1, bias=False)
+        self.vq = ResidualVectorQuantization(
+            nq=nq,
+            dim=dim,
+            codebook_size=bins,
+            codebook_dim=None,
+        )
+
+    def encode(self, xs: mx.array) -> mx.array:
+        if self.input_proj is not None:
+            xs = self.input_proj(xs)
+        return self.vq.encode(xs).swapaxes(0, 1)
+
+    def decode(self, xs: mx.array) -> mx.array:
+        xs = xs.swapaxes(0, 1)
+        quantized = self.vq.decode(xs)
+        if self.output_proj is not None:
+            quantized = self.output_proj(quantized)
+        return quantized
+
+
+class SplitResidualVectorQuantizer(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        input_dim: int | None,
+        output_dim: int | None,
+        nq: int,
+        bins: int
+    ):
+        super().__init__()
+        self._nq = nq
+        self.rvq_first = ResidualVectorQuantizer(
+            dim=dim,
+            input_dim=input_dim,
+            output_dim=output_dim,
+            nq=1,
+            bins=bins,
+            force_projection=True,
+        )
+        self.rvq_rest = ResidualVectorQuantizer(
+            dim=dim,
+            input_dim=input_dim,
+            output_dim=output_dim,
+            nq=nq - 1,
+            bins=bins,
+            force_projection=True
+        )
+
+    def encode(self, xs: mx.array) -> mx.array:
+        codes = self.rvq_first.encode(xs)
+        if self._nq > 1:
+            rest_codes = self.rvq_rest.encode(xs)
+            codes = mx.concat([codes, rest_codes], axis=1)
+        return codes
+
+    def decode(self, xs: mx.array) -> mx.array:
+        quantized = self.rvq_first.decode(xs[:, :1])
+        if self._nq > 1:
+            quantized = quantized + self.rvq_rest.decode(xs[:, 1:])
+        return quantized
---
+++ moshi_mlx/moshi_mlx/modules/seanet.py
@@ -0,0 +1,253 @@
+# Copyright (c) Kyutai, all rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from dataclasses import dataclass
+from .conv import StreamableConv1d, StreamableConvTranspose1d
+
+import mlx.core as mx
+import mlx.nn as nn
+
+
+@dataclass
+class SeanetConfig:
+    dimension: int
+    channels: int
+    causal: bool
+    nfilters: int
+    nresidual_layers: int
+    ratios: list[int]
+    ksize: int
+    residual_ksize: int
+    last_ksize: int
+    dilation_base: int
+    pad_mode: str
+    true_skip: bool
+    compress: int
+
+
+class SeanetResnetBlock(nn.Module):
+    def __init__(self, cfg: SeanetConfig, dim: int, ksizes_and_dilations: list):
+        super().__init__()
+        block = []
+        hidden = dim // cfg.compress
+        for i, (ksize, dilation) in enumerate(ksizes_and_dilations):
+            in_channels = dim if i == 0 else hidden
+            out_channels = dim if i == len(ksizes_and_dilations) - 1 else hidden
+            c = StreamableConv1d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                ksize=ksize,
+                stride=1,
+                dilation=dilation,
+                groups=1,
+                bias=True,
+                causal=cfg.causal,
+                pad_mode=cfg.pad_mode,
+            )
+            block.append(c)
+        self.block = block
+
+        if cfg.true_skip:
+            self.shortcut = None
+        else:
+            self.shortcut = StreamableConv1d(
+                in_channels=dim,
+                out_channels=dim,
+                ksize=1,
+                stride=1,
+                dilation=1,
+                groups=1,
+                bias=True,
+                causal=cfg.causal,
+                pad_mode=cfg.pad_mode,
+            )
+
+    def reset_state(self):
+        if self.shortcut is not None:
+            self.shortcut.reset_state()
+        for b in self.block:
+            b.reset_state()
+
+    def __call__(self, xs: mx.array) -> mx.array:
+        residual = xs
+        for b in self.block:
+            xs = b(nn.elu(xs, alpha=1.0))
+        # TODO(laurent): we might need some streaming additions below.
+        if self.shortcut is None:
+            xs = xs + residual
+        else:
+            xs = xs + self.shortcut(residual)
+        return xs
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, cfg: SeanetConfig, ratio: int, mult: int):
+        super().__init__()
+        residuals = []
+        dilation = 1
+        for _ in range(cfg.nresidual_layers):
+            b = SeanetResnetBlock(
+                cfg,
+                dim=mult * cfg.nfilters,
+                ksizes_and_dilations=[(cfg.residual_ksize, dilation), (1, 1)],
+            )
+            residuals.append(b)
+            dilation *= cfg.dilation_base
+        self.residuals = residuals
+        self.downsample = StreamableConv1d(
+            in_channels=mult * cfg.nfilters,
+            out_channels=mult * cfg.nfilters * 2,
+            ksize=ratio * 2,
+            stride=ratio,
+            dilation=1,
+            groups=1,
+            bias=True,
+            causal=True,
+            pad_mode=cfg.pad_mode,
+        )
+
+    def reset_state(self):
+        self.downsample.reset_state()
+        for r in self.residuals:
+            r.reset_state()
+
+    def __call__(self, xs: mx.array) -> mx.array:
+        for r in self.residuals:
+            xs = r(xs)
+        return self.downsample(nn.elu(xs, alpha=1.0))
+
+
+class SeanetEncoder(nn.Module):
+    def __init__(self, cfg: SeanetConfig):
+        super().__init__()
+        mult = 1
+        self.init_conv1d = StreamableConv1d(
+            in_channels=cfg.channels,
+            out_channels=mult * cfg.nfilters,
+            ksize=cfg.ksize,
+            stride=1,
+            dilation=1,
+            groups=1,
+            bias=True,
+            causal=cfg.causal,
+            pad_mode=cfg.pad_mode,
+        )
+        layers = []
+        for ratio in reversed(cfg.ratios):
+            layers.append(EncoderLayer(cfg, ratio=ratio, mult=mult))
+            mult *= 2
+        self.layers = layers
+        self.final_conv1d = StreamableConv1d(
+            in_channels=mult * cfg.nfilters,
+            out_channels=cfg.dimension,
+            ksize=cfg.last_ksize,
+            stride=1,
+            dilation=1,
+            groups=1,
+            bias=True,
+            causal=cfg.causal,
+            pad_mode=cfg.pad_mode,
+        )
+
+    def reset_state(self):
+        self.init_conv1d.reset_state()
+        self.final_conv1d.reset_state()
+        for layer in self.layers:
+            layer.reset_state()
+
+    def __call__(self, xs: mx.array) -> mx.array:
+        xs = self.init_conv1d(xs)
+        for layer in self.layers:
+            xs = layer(xs)
+        xs = nn.elu(xs, alpha=1.0)
+        return self.final_conv1d(xs)
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, cfg: SeanetConfig, ratio: int, mult: int):
+        super().__init__()
+        self.upsample = StreamableConvTranspose1d(
+            in_channels=mult * cfg.nfilters,
+            out_channels=mult * cfg.nfilters // 2,
+            ksize=ratio * 2,
+            stride=ratio,
+            groups=1,
+            bias=True,
+            causal=cfg.causal,
+        )
+        residuals = []
+        dilation = 1
+        for _ in range(cfg.nresidual_layers):
+            r = SeanetResnetBlock(
+                cfg,
+                dim=mult * cfg.nfilters // 2,
+                ksizes_and_dilations=[(cfg.residual_ksize, dilation), (1, 1)],
+            )
+            residuals.append(r)
+            dilation *= cfg.dilation_base
+        self.residuals = residuals
+
+    def reset_state(self):
+        self.upsample.reset_state()
+        for r in self.residuals:
+            r.reset_state()
+
+    def __call__(self, xs: mx.array) -> mx.array:
+        xs = self.upsample(nn.elu(xs, alpha=1.0))
+        for r in self.residuals:
+            xs = r(xs)
+        return xs
+
+
+class SeanetDecoder(nn.Module):
+    def __init__(self, cfg: SeanetConfig):
+        super().__init__()
+        mult = 1 << len(cfg.ratios)
+        self.init_conv1d = StreamableConv1d(
+            in_channels=cfg.dimension,
+            out_channels=mult * cfg.nfilters,
+            ksize=cfg.ksize,
+            stride=1,
+            dilation=1,
+            groups=1,
+            bias=True,
+            causal=cfg.causal,
+            pad_mode=cfg.pad_mode,
+        )
+        layers = []
+        for ratio in cfg.ratios:
+            layers.append(DecoderLayer(cfg, ratio=ratio, mult=mult))
+            mult //= 2
+        self.layers = layers
+        self.final_conv1d = StreamableConv1d(
+            in_channels=cfg.nfilters,
+            out_channels=cfg.channels,
+            ksize=cfg.last_ksize,
+            stride=1,
+            dilation=1,
+            groups=1,
+            bias=True,
+            causal=cfg.causal,
+            pad_mode=cfg.pad_mode,
+        )
+
+    def reset_state(self):
+        self.init_conv1d.reset_state()
+        self.final_conv1d.reset_state()
+        for layer in self.layers:
+            layer.reset_state()
+
+    def __call__(self, xs: mx.array) -> mx.array:
+        xs = self.init_conv1d(xs)
+        for layer in self.layers:
+            xs = layer(xs)
+        xs = nn.elu(xs, alpha=1.0)
+        return self.final_conv1d(xs)
+
+
+class Seanet(nn.Module):
+    def __init__(self, cfg: SeanetConfig):
+        super().__init__()
+        self.encoder = SeanetEncoder(cfg)
+        self.decoder = SeanetDecoder(cfg)
