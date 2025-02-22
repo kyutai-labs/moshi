779541fba13acc7327827f4d51cd3a91bdf1af4d
Laurent <laurent.mazare@gmail.com>
2025-02-22 12:59:33 +0100

    Streaming test.

--- scripts/mimi_mlx.py
+++ scripts/mimi_mlx.py
@@ -14,6 +14,7 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str)
     parser.add_argument("--hf-repo", type=str, default="kyutai/moshiko-mlx-q4")
+    parser.add_argument("--streaming", action="store_true")
     args = parser.parse_args()
 
     pcm_in, _ = sphn.read(args.input, sample_rate=24000)
@@ -27,10 +28,28 @@ def run():
     model.load_pytorch_weights(model_file, strict=True)
     print("weights loaded")
 
-    codes = model.encode(pcm_in)
-    print(codes.shape)
-    pcm_out = model.decode(codes)
-    print(pcm_out.shape)
+    if args.streaming:
+        chunk_size = 1920
+        pcm_out = []
+        len_ = pcm_in.shape[-1]
+        print("starting streaming conversion")
+        for start_idx in range(0, len_, chunk_size):
+            end_idx = start_idx + chunk_size
+            if end_idx >= len_:
+                break
+            _pcm_in = pcm_in[..., start_idx:end_idx]
+            codes = model.encode(_pcm_in)
+            _pcm_out = model.decode(codes)
+            pcm_out.append(_pcm_out)
+            pct = int(100 * start_idx / len_)
+            print(f"{pct}%", end="\r", flush=True)
+        print()
+        pcm_out = mx.concat(pcm_out, axis=-1)
+    else:
+        codes = model.encode(pcm_in)
+        print(codes.shape)
+        pcm_out = model.decode(codes)
+    print("PCM OUT", pcm_out.shape)
     sphn.write_wav("out.wav", np.array(pcm_out[0]), sample_rate=24000)
 
 if __name__ == "__main__":
