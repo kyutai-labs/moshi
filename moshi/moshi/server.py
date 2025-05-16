26dfb2090399271f9692cdd9a1033403aa6c499f
Luka Darsalia <lukadarsalia13@gmail.com>
2025-05-16 11:34:16 +0400

    Update server.py No need to disable cudagraphs

--- moshi/moshi/server.py
+++ moshi/moshi/server.py
@@ -28,11 +28,6 @@ import os
 import torch._dynamo
 torch._dynamo.config.suppress_errors = True
 
-# Disable CUDA graph capture
-# This is needed because quantized models may use operations
-# that aren't compatible with CUDA graph capture
-os.environ["NO_CUDA_GRAPH"] = "1"
-
 from .client_utils import log
 from .models import loaders, MimiModel, LMModel, LMGen
 from .run_inference import get_condition_tensors
