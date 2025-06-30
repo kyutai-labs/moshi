178b36296e16b9b314b88e33bf925b198aa9f5e6
Laurent Mazare <laurent.mazare@gmail.com>
2025-06-30 09:39:09 +0200

    Fixes for clippy 1.88. (#311)

--- rust/moshi-backend/src/main.rs
+++ rust/moshi-backend/src/main.rs
@@ -92,7 +92,7 @@ fn tracing_init(
     use tracing_subscriber::prelude::*;
 
     let build_info = utils::BuildInfo::new();
-    let file_appender = tracing_appender::rolling::daily(log_dir, format!("log.{}", instance_name));
+    let file_appender = tracing_appender::rolling::daily(log_dir, format!("log.{instance_name}"));
     let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
     let filter = tracing_subscriber::filter::LevelFilter::from_str(log_level)?;
     let mut layers = vec![tracing_subscriber::fmt::layer()
